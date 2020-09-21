f5adfa6f2e1bd07ffc666fe48980aca5a36d6cec
Oleks K <stroga86@gmail.com>
2020-09-21 11:35:16 +0200

    Fixed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-default export (string) {
+export default function (string) {
   if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
   return string.replace(/\s/g, "");
 };
